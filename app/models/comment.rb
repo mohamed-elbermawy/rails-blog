d2a0aa50683b3c831556752a84599ff5381e599f
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-05-26 14:59:08 +0200

    hotwire

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   include Visible
   
   belongs_to :article
+  after_create_commit {broadcast_prepend_to "comments"}
+  after_update_commit {broadcast_replace_to "comments"}
+  after_destroy_commit {broadcast_remove_to "comments"}
 end
